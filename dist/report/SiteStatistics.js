6ff23564f5b33c01d18e85bc6a75af3b08950bb0
MustafaKemal <554074828@qq.com>
2026-05-28 18:24:42 +0800

    fix(report): change edit action to append text instead of overwrite

--- dist/report/SiteStatistics.js
+++ dist/report/SiteStatistics.js
@@ -60,7 +60,7 @@ async function queryStatistics(api, label, verbose) {
     const editResult = await api.postWithToken('csrf', {
         action: 'edit',
         title: REPORT_TITLE,
-        text: wikitext,
+        appendtext: wikitext,
         summary: '机器人：更新站点统计数据',
         bot: true,
         notminor: true,
