210d6bed91f678b162b9d12cb08edb630c905e6d
MustafaKemal <554074828@qq.com>
2026-07-06 00:25:40 +0800

    chore: remove committed dist build artifacts and ignore dist/

--- dist/utils/createApi.d.ts
+++
@@ -1,3 +0,0 @@
-import { MediaWikiApi } from 'wiki-saikou';
-export declare function createZhApi(): MediaWikiApi;
-export declare function createCmApi(): MediaWikiApi;
--- dist/utils/templateRedirects.d.ts
+++
@@ -1,5 +0,0 @@
-import type { MediaWikiApi } from 'wiki-saikou';
-export declare function fetchRedirectsForTemplate(api: MediaWikiApi, templateName: string): Promise<string[]>;
-export declare function buildTemplateNameMap<T extends {
-    templateName: string;
-}>(api: MediaWikiApi, templateConfigs: T[]): Promise<Map<string, T>>;
